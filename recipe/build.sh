821480281dc03809a63278444489e7d9d212e909
Marco Meyer-Conde　　　  メイヤーマコト <37673095+meiyasan@users.noreply.github.com>
2025-10-21 23:13:00 +0900

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,11 +8,12 @@ autoheader
 
 aclocal
 autoconf
-# automake --add-missing
+automake --add-missing
 
 ./configure --enable-pyext --enable-fortranext \
             --prefix=$PREFIX --with-zlib=$PREFIX --with-root=$PREFIX --with-lhapdf=$PREFIX \
-            --with-yoda=$PREFIX --with-qcdnum=$PREFIX --with-fastjet=$PREFIX --with-hoppet=$PREFIX
+            --with-yoda=$PREFIX --with-qcdnum=$PREFIX --with-fastjet=$PREFIX --with-hoppet=$PREFIX \
+            --disable-dependency-tracking
 
 make -j$(nproc)
 lhapdf install CT10nlo
