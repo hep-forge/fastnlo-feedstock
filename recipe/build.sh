b46aebf7ecf1f84cc818f21bdd98488395b18565
Marco Meyer-Conde <marco@tcu.ac.jp>
2025-10-22 10:38:04 +0900

    update buidl

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,12 @@
 cd ./v*/toolkit
 export PYTHONPATH=$SP_DIR
 
-echo 'AUTOMAKE_OPTIONS = no-dependencies' >> Makefile.am
 autoreconf -vfi
 
-./configure --enable-pyext --enable-fortranext \
-            --prefix=$PREFIX --with-zlib=$PREFIX --with-root=$PREFIX --with-lhapdf=$PREFIX \
-            --with-yoda=$PREFIX --with-qcdnum=$PREFIX --with-fastjet=$PREFIX --with-hoppet=$PREFIX \
-            --disable-dependency-tracking
+./configure --prefix=$PREFIX #--enable-pyext --enable-fortranext \
+            # --with-zlib=$PREFIX --with-root=$PREFIX --with-lhapdf=$PREFIX \
+            # --with-yoda=$PREFIX --with-qcdnum=$PREFIX --with-fastjet=$PREFIX --with-hoppet=$PREFIX \
+            # --disable-dependency-tracking
 
 make -j$(nproc)
 lhapdf install CT10nlo
